5b840083ff79643e405a329b3398ade433418565
dev-naeem <naeemz3260@gmail.com>
2026-05-26 13:42:39 -0700

    Add files via upload

---
+++ Models/Booking.cs
@@ -0,0 +1,24 @@
+﻿namespace CarRentalApp.Models
+{
+    public class BookingItem
+    {
+        public Car Car;
+        public int Days;
+
+        public BookingItem(Car car, int days)
+        {
+            Car = car;
+            Days = days;
+        }
+
+        public double TotalPrice()
+        {
+            return Days * Car.PricePerDay;
+        }
+
+        public override string ToString()
+        {
+            return $"{Car.Name} - {Days} Days - Rs {TotalPrice()}";
+        }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace CarRentalApp.Models
+{
+    public class Car
+    {
+        public string Name;
+        public string Category;
+        public double PricePerDay;
+        public int Available;
+
+        public Car(string name, string category, double pricePerDay, int available)
+        {
+            Name = name;
+            Category = category;
+            PricePerDay = pricePerDay;
+            Available = available;
+        }
+    }
+}
